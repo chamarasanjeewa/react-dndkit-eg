70abfcd647ec00168af30414e8b6d52785f4a58b
Chamara sanjeewa <chamara@babyjourney.se>
2023-01-25 09:58:20 +0530

    working on sorting items

--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,35 @@ import HorizontalEg from "./../components/horizontalEg/index";
 import "./../styles/App.css";
 import GridSwap from "./gridSwapEg";
 import React, { useReducer } from "react";
+import {
+  DndContext,
+  closestCenter,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import { SortableItem } from "./sortableItem";
+
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
 const initialState = [
   {
     id: 1,
     sortOrder: 1,
     title: "Content collection",
     items: [
-      { id: "1", text: "Pregnancy" },
-      { id: "2", text: "Common questions" },
-      { id: "3", text: "Dietary advice" },
-      { id: "4", text: "Give birth" },
-      { id: "5", text: "Breast feeding" },
+      { id: "1", text: "Pregnancy", sortOrder: 1 },
+      { id: "2", text: "Common questions", sortOrder: 2 },
+      { id: "3", text: "Dietary advice", sortOrder: 3 },
+      { id: "4", text: "Give birth", sortOrder: 4 },
+      { id: "5", text: "Breast feeding", sortOrder: 5 },
+      { id: "6", text: "another random", sortorder: 6 },
+      { id: "7", text: "new item ", sortOrder: 7 },
     ],
   },
   {
@@ -21,37 +39,37 @@ const initialState = [
     sortOrder: 2,
     title: "Pregnancy and child",
     items: [
-      { id: "1", text: "Weekly info pregnancy" },
-      { id: "2", text: "Monthly info pregnancy" },
+      { id: "1", text: "Weekly info pregnancy", sortOrder: 1 },
+      { id: "2", text: "Monthly info pregnancy", sortOrder: 2 },
     ],
   },
-  {
-    id: 3,
-    sortOrder: 3,
-    title: "Blog posts",
-    items: [
-      { id: "1", text: "Weekly" },
-      { id: "2", text: "Monthly" },
-    ],
-  },
-  {
-    id: 3,
-    sortOrder: 4,
-    title: "Tools",
-    items: [
-      { id: "1", text: "Mood tracker" },
-      { id: "2", text: "Weight cal" },
-    ],
-  },
-  {
-    id: 5,
-    sortOrder: 5,
-    title: "Check lists",
-    items: [
-      { id: "1", text: "Pregnancy" },
-      { id: "2", text: "Child" },
-    ],
-  },
+  // {
+  //   id: 3,
+  //   sortOrder: 3,
+  //   title: "Blog posts",
+  //   items: [
+  //     { id: "1", text: "Weekly" },
+  //     { id: "2", text: "Monthly" },
+  //   ],
+  // },
+  // {
+  //   id: 3,
+  //   sortOrder: 4,
+  //   title: "Tools",
+  //   items: [
+  //     { id: "1", text: "Mood tracker" },
+  //     { id: "2", text: "Weight cal" },
+  //   ],
+  // },
+  // {
+  //   id: 5,
+  //   sortOrder: 5,
+  //   title: "Check lists",
+  //   items: [
+  //     { id: "1", text: "Pregnancy" },
+  //     { id: "2", text: "Child" },
+  //   ],
+  // },
 ];
 
 const reducer = (state, action) => {
@@ -77,26 +95,93 @@ const reducer = (state, action) => {
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
-  return (
-    <div className={["App"].join(" ")}>
-      <div className="appContainer">
-        <div>
-          <p>Nested array of object</p>
-          <HorizontalEg />
-        </div>
-
-        <div className="grid-egs">
-          {/* <div>
-            <p className='label-text'>Basic Grid with Drag Handle</p>
-            <GridEg />
-          </div> */}
-          <div style={{ marginTop: "48px" }}>
-            <p className="label-text">Grid Swap with Activation Constraint</p>
-            <GridSwap state={state} dispatch={dispatch} />
-          </div>
-        </div>
-      </div>
-    </div>
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
   );
+  return (
+    <DndContext
+      sensors={sensors}
+      collisionDetection={closestCenter}
+      onDragEnd={handleDragEnd}
+    >
+      <SortableContext items={state} strategy={verticalListSortingStrategy}>
+        {state
+          .sort((a, b) => a.sortOrder - b.sortOrder)
+          .map(item => (
+            <SortableItem key={item.id} id={item.id}>
+              <div className="label-text">
+                <span>{item.title}</span>
+                <div
+                  style={{
+                    _marginLeft: "5px",
+                    get marginLeft() {
+                      return this._marginLeft;
+                    },
+                    set marginLeft(value) {
+                      this._marginLeft = value;
+                    },
+                    display: "inline-block",
+                    backgroundColor: "red",
+                  }}
+                  onClick={() => {
+                    console.log("edit clicked...");
+                  }}
+                >
+                  edit
+                </div>
+                <span
+                  style={{ marginLeft: "5px" }}
+                  onClick={() => {
+                    console.log("delete clicked...");
+                  }}
+                >
+                  delete
+                </span>
+              </div>
+              <GridSwap dispatch={dispatch} state={item} />
+            </SortableItem>
+          ))}
+      </SortableContext>
+    </DndContext>
+    // <div className={["App"].join(" ")}>
+    //   <div className="appContainer">
+    //     <div>
+    //       <p>Nested array of object</p>
+    //       <HorizontalEg />
+    //     </div>
+
+    //     <div className="grid-egs">
+    //       {/* <div>
+    //         <p className='label-text'>Basic Grid with Drag Handle</p>
+    //         <GridEg />
+    //       </div> */}
+    //       <div style={{ marginTop: "48px" }}>
+    //         <p className="label-text">Grid Swap with Activation Constraint</p>
+    //         <GridSwap state={state[0]} dispatch={dispatch} />
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
+  );
+}
+
+function handleDragEnd(event) {
+  const { active, over } = event;
+
+  // if (active.id !== over.id) {
+  //   const oldIndex = state.indexOf(active.id);
+  //   const newIndex = state.indexOf(over.id);
+  //   const newOrder = arrayMove(state, oldIndex, newIndex);
+  //   dispatch({
+  //     type: "sortSection",
+  //     payload: {
+  //       id: state.id,
+  //       items: newOrder,
+  //     },
+  //   });
+  // }
 }
 export default App;
--- src/components/gridSwapEg/index.js
+++ src/components/gridSwapEg/index.js
@@ -1,29 +1,22 @@
-import { useState } from 'react';
-import {SortableItem} from '../sortableItem';
+import { SortableItem } from "../sortableItem";
 import {
   DndContext,
   KeyboardSensor,
   PointerSensor,
   useSensor,
   useSensors,
-  closestCenter
-} from '@dnd-kit/core';
-import {  
+  closestCenter,
+} from "@dnd-kit/core";
+import {
   SortableContext,
   sortableKeyboardCoordinates,
   rectSwappingStrategy,
   arraySwap,
-} from '@dnd-kit/sortable';
-import './../../styles/App.css';
-import { ColorArrayMapping } from '../../shared/constants';
+} from "@dnd-kit/sortable";
+import "./../../styles/App.css";
+import { ColorArrayMapping } from "../../shared/constants";
 
 function GridSwap({ state, dispatch }) {
-  const [items, setItems] = useState([
-    {id:'1', text: 'A'},
-    {id:'2', text: 'B'},
-    {id:'3', text: 'C'},
-    {id:'4', text: 'D'}
-  ]);
 
   const sensors = useSensors(
     useSensor(PointerSensor, { activationConstraint: { distance: 5 } }),
@@ -31,50 +24,69 @@ function GridSwap({ state, dispatch }) {
       coordinateGetter: sortableKeyboardCoordinates,
     })
   );
-  
-  const handleGridClick = (values) => {
-    console.log(values.text); 
-    window.alert(values.text)
+
+  const handleGridClick = values => {
+    console.log(values.text);
+    window.alert(values.text);
   };
-  
+
   return (
     <div className="App">
-       <div className={'gridcontainer'}>
-        <DndContext id={'grid-dnd'} 
-          onDragEnd={handleDragEnd} 
+      <div className={"gridcontainer"}>
+        <DndContext
+          id={"grid-dnd"}
+          onDragEnd={handleDragEnd}
           sensors={sensors}
           collisionDetection={closestCenter}
         >
-            <SortableContext 
-              strategy={rectSwappingStrategy}
-              id={'grid-sort-context'}
-              items={items.map((i) => i?.id)}
-              reorderItems={arraySwap}
-            >
-              {items.map(value => {
-                const bgColor = ColorArrayMapping[value?.id]
+          <SortableContext
+            strategy={rectSwappingStrategy}
+            id={"grid-sort-context"}
+            items={state?.items.map(i => i?.id)}
+            reorderItems={arraySwap}
+          >
+            {state?.items
+              .sort((a, b) => a.sortOrder - b.sortOrder)
+              .map(value => {
+                const bgColor = ColorArrayMapping[value?.id];
                 return (
-                  <SortableItem key={value?.id} id={value?.id} >
-                    <div className={[`bg${bgColor}`, `gridItem`].join(' ')} onClick={() =>handleGridClick(value)}>
+                  <SortableItem key={value?.id} id={value?.id}>
+                    <div
+                      className={[`bg${bgColor}`, `gridItem`].join(" ")}
+                      onClick={() => handleGridClick(value)}
+                    >
                       <p>{value?.text}</p>
-                      <p className='clickme'>click me</p>
+                      <p className="clickme">click me</p>
                     </div>
                   </SortableItem>
-                )
+                );
               })}
-            </SortableContext>
-          </DndContext>
-       </div>
+          </SortableContext>
+        </DndContext>
+      </div>
     </div>
   );
 
   function handleDragEnd(event) {
-    const {active, over} = event;
+    debugger;
+    const { active, over } = event;
     if (active.id !== over.id) {
-      setItems((items) => {
-        const oldIndex = (items.map(i => i.id)).indexOf(active.id);
-        const newIndex = (items.map(i => i.id)).indexOf(over.id);
-        return arraySwap(items, oldIndex, newIndex)
+      const oldItem = state?.items.find(x => x.id === active.id);
+      const newItem = state?.items.find(x => x.id === over.id);
+      const newOrder = state?.items.map(x => {
+        if (x.id === oldItem.id) {
+          return { ...x, sortOrder: newItem?.sortOrder };
+        } else if (x.id === newItem.id) {
+          return { ...x, sortOrder: oldItem?.sortOrder };
+        } else return x;
+      });
+
+      dispatch({
+        type: "sortItem",
+        payload: {
+          id: state.id,
+          items: newOrder,
+        },
       });
     }
   }
